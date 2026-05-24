895c144999c5b5eca74e7cda4e7ed1f68c135054
jenniferkim-0704 <jennifer060704@gmail.com>
2026-05-24 22:18:33 +0900

    [수정] conda 환경 이름

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -50,6 +50,7 @@ partial class Form1
         btnLoadImages = new Button();
         btnLoadCatalog = new Button();
         tabp_Serve = new TabPage();
+        txtCondaEnv = new TextBox();
         txtLog = new TextBox();
         btnTrain = new Button();
         tabcMain.SuspendLayout();
@@ -64,10 +65,10 @@ partial class Form1
         tabcMain.Controls.Add(tabPage1);
         tabcMain.Controls.Add(tabp_Serve);
         tabcMain.Location = new Point(0, 0);
-        tabcMain.Margin = new Padding(4, 3, 4, 3);
+        tabcMain.Margin = new Padding(3, 2, 3, 2);
         tabcMain.Name = "tabcMain";
         tabcMain.SelectedIndex = 0;
-        tabcMain.Size = new Size(2351, 1094);
+        tabcMain.Size = new Size(1511, 684);
         tabcMain.TabIndex = 0;
         // 
         // tabPage1
@@ -91,11 +92,11 @@ partial class Form1
         tabPage1.Controls.Add(pic_DkScreen);
         tabPage1.Controls.Add(btnLoadImages);
         tabPage1.Controls.Add(btnLoadCatalog);
-        tabPage1.Location = new Point(8, 46);
-        tabPage1.Margin = new Padding(4, 3, 4, 3);
+        tabPage1.Location = new Point(4, 29);
+        tabPage1.Margin = new Padding(3, 2, 3, 2);
         tabPage1.Name = "tabPage1";
-        tabPage1.Padding = new Padding(4, 3, 4, 3);
-        tabPage1.Size = new Size(2335, 1040);
+        tabPage1.Padding = new Padding(3, 2, 3, 2);
+        tabPage1.Size = new Size(1503, 651);
         tabPage1.TabIndex = 0;
         tabPage1.Text = "tabPage1";
         tabPage1.UseVisualStyleBackColor = true;
@@ -104,9 +105,10 @@ partial class Form1
         // 
         lbl_ThrottleV.AutoSize = true;
         lbl_ThrottleV.Font = new Font("맑은 고딕", 15F);
-        lbl_ThrottleV.Location = new Point(2071, 115);
+        lbl_ThrottleV.Location = new Point(1331, 72);
+        lbl_ThrottleV.Margin = new Padding(2, 0, 2, 0);
         lbl_ThrottleV.Name = "lbl_ThrottleV";
-        lbl_ThrottleV.Size = new Size(45, 54);
+        lbl_ThrottleV.Size = new Size(29, 35);
         lbl_ThrottleV.TabIndex = 19;
         lbl_ThrottleV.Text = "0";
         lbl_ThrottleV.Click += label5_Click;
@@ -115,9 +117,10 @@ partial class Form1
         // 
         lbl_Throttle.AutoSize = true;
         lbl_Throttle.Font = new Font("맑은 고딕", 15F);
-        lbl_Throttle.Location = new Point(1882, 115);
+        lbl_Throttle.Location = new Point(1210, 72);
+        lbl_Throttle.Margin = new Padding(2, 0, 2, 0);
         lbl_Throttle.Name = "lbl_Throttle";
-        lbl_Throttle.Size = new Size(173, 54);
+        lbl_Throttle.Size = new Size(108, 35);
         lbl_Throttle.TabIndex = 18;
         lbl_Throttle.Text = "Throttle:";
         // 
@@ -125,9 +128,10 @@ partial class Form1
         // 
         lbl_AngleV.AutoSize = true;
         lbl_AngleV.Font = new Font("맑은 고딕", 15F);
-        lbl_AngleV.Location = new Point(1758, 115);
+        lbl_AngleV.Location = new Point(1130, 72);
+        lbl_AngleV.Margin = new Padding(2, 0, 2, 0);
         lbl_AngleV.Name = "lbl_AngleV";
-        lbl_AngleV.Size = new Size(45, 54);
+        lbl_AngleV.Size = new Size(29, 35);
         lbl_AngleV.TabIndex = 17;
         lbl_AngleV.Text = "0";
         // 
@@ -135,9 +139,10 @@ partial class Form1
         // 
         lbl_Angle.AutoSize = true;
         lbl_Angle.Font = new Font("맑은 고딕", 15F);
-        lbl_Angle.Location = new Point(1616, 115);
+        lbl_Angle.Location = new Point(1039, 72);
+        lbl_Angle.Margin = new Padding(2, 0, 2, 0);
         lbl_Angle.Name = "lbl_Angle";
-        lbl_Angle.Size = new Size(136, 54);
+        lbl_Angle.Size = new Size(84, 35);
         lbl_Angle.TabIndex = 16;
         lbl_Angle.Text = "Angle:";
         // 
@@ -145,9 +150,10 @@ partial class Form1
         // 
         lbl_FrameV.AutoSize = true;
         lbl_FrameV.Font = new Font("맑은 고딕", 15F);
-        lbl_FrameV.Location = new Point(1438, 115);
+        lbl_FrameV.Location = new Point(924, 72);
+        lbl_FrameV.Margin = new Padding(2, 0, 2, 0);
         lbl_FrameV.Name = "lbl_FrameV";
-        lbl_FrameV.Size = new Size(45, 54);
+        lbl_FrameV.Size = new Size(29, 35);
         lbl_FrameV.TabIndex = 15;
         lbl_FrameV.Text = "0";
         // 
@@ -155,86 +161,96 @@ partial class Form1
         // 
         lbl_Frame.AutoSize = true;
         lbl_Frame.Font = new Font("맑은 고딕", 15F);
-        lbl_Frame.Location = new Point(1289, 115);
+        lbl_Frame.Location = new Point(829, 72);
+        lbl_Frame.Margin = new Padding(2, 0, 2, 0);
         lbl_Frame.Name = "lbl_Frame";
-        lbl_Frame.Size = new Size(143, 54);
+        lbl_Frame.Size = new Size(89, 35);
         lbl_Frame.TabIndex = 14;
         lbl_Frame.Text = "Frame:";
         // 
         // list_FileCheck
         // 
         list_FileCheck.FormattingEnabled = true;
-        list_FileCheck.Location = new Point(37, 664);
+        list_FileCheck.Location = new Point(24, 415);
+        list_FileCheck.Margin = new Padding(2);
         list_FileCheck.Name = "list_FileCheck";
-        list_FileCheck.Size = new Size(636, 164);
+        list_FileCheck.Size = new Size(410, 104);
         list_FileCheck.TabIndex = 13;
         // 
         // tbar_Dk
         // 
-        tbar_Dk.Location = new Point(37, 556);
+        tbar_Dk.Location = new Point(24, 348);
+        tbar_Dk.Margin = new Padding(2);
         tbar_Dk.Name = "tbar_Dk";
-        tbar_Dk.Size = new Size(996, 90);
+        tbar_Dk.Size = new Size(640, 56);
         tbar_Dk.TabIndex = 12;
         // 
         // btn_Retry
         // 
-        btn_Retry.Location = new Point(851, 864);
+        btn_Retry.Location = new Point(547, 540);
+        btn_Retry.Margin = new Padding(2);
         btn_Retry.Name = "btn_Retry";
-        btn_Retry.Size = new Size(150, 73);
+        btn_Retry.Size = new Size(96, 46);
         btn_Retry.TabIndex = 11;
         btn_Retry.Text = "초기화";
         btn_Retry.UseVisualStyleBackColor = true;
         // 
         // btn_Find
         // 
-        btn_Find.Location = new Point(648, 859);
+        btn_Find.Location = new Point(417, 537);
+        btn_Find.Margin = new Padding(2);
         btn_Find.Name = "btn_Find";
-        btn_Find.Size = new Size(150, 78);
+        btn_Find.Size = new Size(96, 49);
         btn_Find.TabIndex = 10;
         btn_Find.Text = "찾기";
         btn_Find.UseVisualStyleBackColor = true;
         // 
         // btn_Del
         // 
-        btn_Del.Location = new Point(63, 869);
+        btn_Del.Location = new Point(40, 543);
+        btn_Del.Margin = new Padding(2);
         btn_Del.Name = "btn_Del";
-        btn_Del.Size = new Size(150, 73);
+        btn_Del.Size = new Size(96, 46);
         btn_Del.TabIndex = 9;
         btn_Del.Text = "삭제";
         btn_Del.UseVisualStyleBackColor = true;
         // 
         // btn_SmallR
         // 
-        btn_SmallR.Location = new Point(676, 461);
+        btn_SmallR.Location = new Point(435, 288);
+        btn_SmallR.Margin = new Padding(2);
         btn_SmallR.Name = "btn_SmallR";
-        btn_SmallR.Size = new Size(238, 73);
+        btn_SmallR.Size = new Size(153, 46);
         btn_SmallR.TabIndex = 8;
         btn_SmallR.Text = ">>";
         btn_SmallR.UseVisualStyleBackColor = true;
         // 
         // btn_Stop
         // 
-        btn_Stop.Location = new Point(419, 466);
+        btn_Stop.Location = new Point(269, 291);
+        btn_Stop.Margin = new Padding(2);
         btn_Stop.Name = "btn_Stop";
-        btn_Stop.Size = new Size(236, 73);
+        btn_Stop.Size = new Size(152, 46);
         btn_Stop.TabIndex = 7;
         btn_Stop.Text = "stop";
         btn_Stop.UseVisualStyleBackColor = true;
         // 
         // btn_SmallL
         // 
-        btn_SmallL.Location = new Point(167, 466);
+        btn_SmallL.Location = new Point(107, 291);
+        btn_SmallL.Margin = new Padding(2);
         btn_SmallL.Name = "btn_SmallL";
-        btn_SmallL.Size = new Size(237, 73);
+        btn_SmallL.Size = new Size(152, 46);
         btn_SmallL.TabIndex = 6;
         btn_SmallL.Text = "<<";
         btn_SmallL.UseVisualStyleBackColor = true;
         // 
         // btn_BigL
         // 
-        btn_BigL.Location = new Point(37, 115);
+        btn_BigL.Location = new Point(24, 72);
+        btn_BigL.Margin = new Padding(2);
         btn_BigL.Name = "btn_BigL";
-        btn_BigL.Size = new Size(124, 424);
+        btn_BigL.Size = new Size(80, 265);
         btn_BigL.TabIndex = 5;
         btn_BigL.Text = "<";
         btn_BigL.UseVisualStyleBackColor = true;
@@ -242,37 +258,39 @@ partial class Form1
         // 
         // btn_BigR
         // 
-        btn_BigR.Location = new Point(920, 115);
+        btn_BigR.Location = new Point(591, 72);
+        btn_BigR.Margin = new Padding(2);
         btn_BigR.Name = "btn_BigR";
-        btn_BigR.Size = new Size(124, 424);
+        btn_BigR.Size = new Size(80, 265);
         btn_BigR.TabIndex = 4;
         btn_BigR.Text = ">";
         btn_BigR.UseVisualStyleBackColor = true;
         // 
         // pic_DkScreen
         // 
-        pic_DkScreen.Location = new Point(167, 115);
+        pic_DkScreen.Location = new Point(107, 72);
+        pic_DkScreen.Margin = new Padding(2);
         pic_DkScreen.Name = "pic_DkScreen";
-        pic_DkScreen.Size = new Size(747, 340);
+        pic_DkScreen.Size = new Size(480, 212);
         pic_DkScreen.TabIndex = 2;
         pic_DkScreen.TabStop = false;
         // 
         // btnLoadImages
         // 
-        btnLoadImages.Location = new Point(265, 10);
-        btnLoadImages.Margin = new Padding(4);
+        btnLoadImages.Location = new Point(170, 6);
+        btnLoadImages.Margin = new Padding(3, 2, 3, 2);
         btnLoadImages.Name = "btnLoadImages";
-        btnLoadImages.Size = new Size(245, 83);
+        btnLoadImages.Size = new Size(158, 52);
         btnLoadImages.TabIndex = 1;
         btnLoadImages.Text = "이미지 파일 \r\n불러오기\r\n";
         btnLoadImages.UseVisualStyleBackColor = true;
         // 
         // btnLoadCatalog
         // 
-        btnLoadCatalog.Location = new Point(11, 10);
-        btnLoadCatalog.Margin = new Padding(4);
+        btnLoadCatalog.Location = new Point(7, 6);
+        btnLoadCatalog.Margin = new Padding(3, 2, 3, 2);
         btnLoadCatalog.Name = "btnLoadCatalog";
-        btnLoadCatalog.Size = new Size(245, 83);
+        btnLoadCatalog.Size = new Size(158, 52);
         btnLoadCatalog.TabIndex = 0;
         btnLoadCatalog.Text = "카탈로그 파일 \r\n불러오기\r\n";
         btnLoadCatalog.UseVisualStyleBackColor = true;
@@ -280,33 +298,43 @@ partial class Form1
         // 
         // tabp_Serve
         // 
+        tabp_Serve.Controls.Add(txtCondaEnv);
         tabp_Serve.Controls.Add(txtLog);
         tabp_Serve.Controls.Add(btnTrain);
-        tabp_Serve.Location = new Point(8, 46);
-        tabp_Serve.Margin = new Padding(4, 3, 4, 3);
+        tabp_Serve.Location = new Point(4, 29);
+        tabp_Serve.Margin = new Padding(3, 2, 3, 2);
         tabp_Serve.Name = "tabp_Serve";
-        tabp_Serve.Padding = new Padding(4, 3, 4, 3);
-        tabp_Serve.Size = new Size(2335, 1040);
+        tabp_Serve.Padding = new Padding(3, 2, 3, 2);
+        tabp_Serve.Size = new Size(1503, 651);
         tabp_Serve.TabIndex = 1;
         tabp_Serve.Text = "tabPage2";
         tabp_Serve.UseVisualStyleBackColor = true;
         // 
+        // txtCondaEnv
+        // 
+        txtCondaEnv.ForeColor = Color.Silver;
+        txtCondaEnv.Location = new Point(501, 36);
+        txtCondaEnv.Name = "txtCondaEnv";
+        txtCondaEnv.Size = new Size(268, 27);
+        txtCondaEnv.TabIndex = 2;
+        txtCondaEnv.Text = "Conda 환경 이름을 쓰세요";
+        txtCondaEnv.Enter += txtCondaEnv_Enter;
+        txtCondaEnv.Leave += txtCondaEnv_Leave;
+        // 
         // txtLog
         // 
-        txtLog.Location = new Point(13, 161);
-        txtLog.Margin = new Padding(4, 5, 4, 5);
+        txtLog.Location = new Point(8, 101);
         txtLog.Multiline = true;
         txtLog.Name = "txtLog";
         txtLog.ScrollBars = ScrollBars.Vertical;
-        txtLog.Size = new Size(1848, 824);
+        txtLog.Size = new Size(1189, 516);
         txtLog.TabIndex = 1;
         // 
         // btnTrain
         // 
-        btnTrain.Location = new Point(13, 8);
-        btnTrain.Margin = new Padding(4, 5, 4, 5);
+        btnTrain.Location = new Point(8, 5);
         btnTrain.Name = "btnTrain";
-        btnTrain.Size = new Size(573, 143);
+        btnTrain.Size = new Size(368, 89);
         btnTrain.TabIndex = 0;
         btnTrain.Text = "학습 시작";
         btnTrain.UseVisualStyleBackColor = true;
@@ -314,11 +342,11 @@ partial class Form1
         // 
         // Form1
         // 
-        AutoScaleDimensions = new SizeF(14F, 32F);
+        AutoScaleDimensions = new SizeF(9F, 20F);
         AutoScaleMode = AutoScaleMode.Font;
-        ClientSize = new Size(2349, 1080);
+        ClientSize = new Size(1237, 659);
         Controls.Add(tabcMain);
-        Margin = new Padding(4, 3, 4, 3);
+        Margin = new Padding(3, 2, 3, 2);
         Name = "Form1";
         Text = ".";
         tabcMain.ResumeLayout(false);
@@ -335,9 +363,6 @@ partial class Form1
 
     private TabControl tabcMain;
     private TabPage tabPage1;
-    private TabPage tabp_Serve;
-    private TextBox txtLog;
-    private Button btnTrain;
     private Button btnLoadCatalog;
     private Button btnLoadImages;
     private PictureBox pic_DkScreen;
@@ -358,4 +383,8 @@ partial class Form1
     private Label lbl_Angle;
     private Label lbl_FrameV;
     private Label lbl_Frame;
+    private TabPage tabp_Serve;
+    private TextBox txtLog;
+    private Button btnTrain;
+    private TextBox txtCondaEnv;
 }
